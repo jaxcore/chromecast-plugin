cbae2253ca2f92bed80fb73aec79e92acc5c65d4
Dan Steinman <dansteinman@gmail.com>
2019-11-07 19:56:49 -0500

    convert adapter to Adapter class

--- chromecast-adapter.js
+++ chromecast-adapter.js
@@ -1,17 +1,18 @@
-function getDefaultState() {
-	return {
-	};
-}
+const Adapter = require('jaxcore-plugin').Adapter;
 
-function chromecastAdapter() {
-	const {spin} = this.devices;
-	const {chromecast} = this.services;
-	const {theme} = this;
-	spin.rotateRainbow(2);
-	spin.lightsOff();
+class ChromecastAdapter extends Adapter {
+	static getDefaultState() {
+		return {};
+	}
 	
-	this.setEvents({
-		chromecast: {
+	constructor(config, theme, devices, services) {
+		super(config, theme, devices, services);
+		const {spin} = devices;
+		const {chromecast} = services;
+		spin.rotateRainbow(2);
+		spin.lightsOff();
+		
+		this.addEvents(chromecast, {
 			play: function() {
 				this.log('play');
 				spin.flash(theme.success);
@@ -40,8 +41,9 @@ function chromecastAdapter() {
 					spin.scale(chromecast.state.volumePercent, theme.low, theme.high, theme.middle);
 				}
 			}
-		},
-		spin: {
+		});
+		
+		this.addEvents(spin, {
 			spin: function (diff, spinTime) {
 				this.log('spin rotate', diff, spinTime);
 				if (chromecast.state.muted) {
@@ -64,23 +66,160 @@ function chromecastAdapter() {
 				this.log('button', pushed);
 				if (pushed) chromecast.togglePlayPause();
 			}
-		}
-	});
+		});
+		
+		// this.setEvents({
+		// 	chromecast: {
+		// 		play: function() {
+		// 			this.log('play');
+		// 			spin.flash(theme.success);
+		// 		},
+		// 		stop: function() {
+		// 			this.log('stop');
+		// 			spin.flash(theme.error);
+		// 		},
+		// 		pause: function() {
+		// 			this.log('paused');
+		// 			spin.flash(theme.secondary);
+		// 		},
+		// 		muted: function(muted) {
+		// 			this.log('muted', muted);
+		// 			if (chromecast.state.muted) {
+		// 				spin.scale(chromecast.state.volumePercent, theme.tertiary, theme.tertiary, theme.middle);
+		// 			} else {
+		// 				spin.scale(chromecast.state.volumePercent, theme.low, theme.high, theme.middle);
+		// 			}
+		// 		},
+		// 		volume: function(volumePercent, volume) {
+		// 			this.log('volume', volumePercent, volume);
+		// 			if (chromecast.state.muted) {
+		// 				spin.scale(chromecast.state.volumePercent, theme.tertiary, theme.tertiary, theme.middle);
+		// 			} else {
+		// 				spin.scale(chromecast.state.volumePercent, theme.low, theme.high, theme.middle);
+		// 			}
+		// 		}
+		// 	},
+		// 	spin: {
+		// 		spin: function (diff, spinTime) {
+		// 			this.log('spin rotate', diff, spinTime);
+		// 			if (chromecast.state.muted) {
+		// 				spin.scale(chromecast.state.volumePercent, theme.tertiary, theme.tertiary, theme.middle);
+		// 			} else {
+		// 				if (spinTime > 200) {
+		// 					chromecast.changeVolume(diff);
+		// 				} else {
+		// 					diff = spin.buffer(diff, 1, 0);
+		// 					if (diff !== 0) chromecast.changeVolume(diff * 2);
+		// 				}
+		// 				//sspin.scale(volumePercent, theme.low, theme.high, theme.middle);
+		// 			}
+		// 		},
+		// 		knob: function (pushed) {
+		// 			this.log('knob', pushed);
+		// 			if (pushed) chromecast.toggleMuted();
+		// 		},
+		// 		button: function (pushed) {
+		// 			this.log('button', pushed);
+		// 			if (pushed) chromecast.togglePlayPause();
+		// 		}
+		// 	}
+		// });
+	}
+	
+	static getServicesConfig(adapterConfig) {
+		console.log('chromecastAdapter getServicesConfig', adapterConfig);
+		return {
+			chromecast: {
+				name: 'Family room TV'
+				// adapterConfig.services.chromecast
+			}
+		};
+	};
 }
 
-chromecastAdapter.getServicesConfig = function(adapterConfig) {
-	console.log('chromecastAdapter getServicesConfig', adapterConfig);
-	
-	let servicesConfig = {
-		chromecast: {
-			name: 'Family room TV'
-			// adapterConfig.services.chromecast
-		}
-	};
-	
-	return servicesConfig;
-};
+// function getDefaultState() {
+// 	return {
+// 	};
+// }
+//
+// function chromecastAdapter() {
+// 	const {spin} = this.devices;
+// 	const {chromecast} = this.services;
+// 	const {theme} = this;
+// 	spin.rotateRainbow(2);
+// 	spin.lightsOff();
+//
+// 	this.setEvents({
+// 		chromecast: {
+// 			play: function() {
+// 				this.log('play');
+// 				spin.flash(theme.success);
+// 			},
+// 			stop: function() {
+// 				this.log('stop');
+// 				spin.flash(theme.error);
+// 			},
+// 			pause: function() {
+// 				this.log('paused');
+// 				spin.flash(theme.secondary);
+// 			},
+// 			muted: function(muted) {
+// 				this.log('muted', muted);
+// 				if (chromecast.state.muted) {
+// 					spin.scale(chromecast.state.volumePercent, theme.tertiary, theme.tertiary, theme.middle);
+// 				} else {
+// 					spin.scale(chromecast.state.volumePercent, theme.low, theme.high, theme.middle);
+// 				}
+// 			},
+// 			volume: function(volumePercent, volume) {
+// 				this.log('volume', volumePercent, volume);
+// 				if (chromecast.state.muted) {
+// 					spin.scale(chromecast.state.volumePercent, theme.tertiary, theme.tertiary, theme.middle);
+// 				} else {
+// 					spin.scale(chromecast.state.volumePercent, theme.low, theme.high, theme.middle);
+// 				}
+// 			}
+// 		},
+// 		spin: {
+// 			spin: function (diff, spinTime) {
+// 				this.log('spin rotate', diff, spinTime);
+// 				if (chromecast.state.muted) {
+// 					spin.scale(chromecast.state.volumePercent, theme.tertiary, theme.tertiary, theme.middle);
+// 				} else {
+// 					if (spinTime > 200) {
+// 						chromecast.changeVolume(diff);
+// 					} else {
+// 						diff = spin.buffer(diff, 1, 0);
+// 						if (diff !== 0) chromecast.changeVolume(diff * 2);
+// 					}
+// 					//sspin.scale(volumePercent, theme.low, theme.high, theme.middle);
+// 				}
+// 			},
+// 			knob: function (pushed) {
+// 				this.log('knob', pushed);
+// 				if (pushed) chromecast.toggleMuted();
+// 			},
+// 			button: function (pushed) {
+// 				this.log('button', pushed);
+// 				if (pushed) chromecast.togglePlayPause();
+// 			}
+// 		}
+// 	});
+// }
+//
+// chromecastAdapter.getServicesConfig = function(adapterConfig) {
+// 	console.log('chromecastAdapter getServicesConfig', adapterConfig);
+//
+// 	let servicesConfig = {
+// 		chromecast: {
+// 			name: 'Family room TV'
+// 			// adapterConfig.services.chromecast
+// 		}
+// 	};
+//
+// 	return servicesConfig;
+// };
+//
+// chromecastAdapter.getDefaultState = getDefaultState;
 
-chromecastAdapter.getDefaultState = getDefaultState;
-
-module.exports = chromecastAdapter;
+module.exports = ChromecastAdapter;
